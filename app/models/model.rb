a532c17d49fcdf1faa9db7f60ddd637bf48275e4
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-19 20:42:04 +0530

    Add relationshipbetween user and tweet

--- app/models/model.rb
+++ app/models/model.rb
@@ -3,4 +3,6 @@ class Model < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tweeets
 end
