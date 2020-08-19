36b0058ce52e87d5015bc67518e037f17b0ffaa2
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-08-19 17:52:17 +0530

    Add devise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_101817) do
+ActiveRecord::Schema.define(version: 2020_08_19_122024) do
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
